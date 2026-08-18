d233ad767421f012c4484c893b81b25ff9398e32
chaevsfe <pie@duck.com>
2026-08-17 19:26:34 -0700

    Delete the empty section compiler mixin

--- fabric/src/main/java/com/railwayteam/railways/fabric_mixin/client/MixinSectionCompiler.java
+++
@@ -1,8 +0,0 @@
-package com.railwayteam.railways.fabric_mixin.client;
-
-import net.minecraft.client.renderer.chunk.SectionCompiler;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(SectionCompiler.class)
-public class MixinSectionCompiler {
-}
