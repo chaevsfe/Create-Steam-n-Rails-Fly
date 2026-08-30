148929f9f212ff95802a471b77fcc59829c837a7
chaevsfe <pie@duck.com>
2026-08-30 18:39:03 -0400

    Draw wrench value box icons from the railways atlas on all paths

--- common/src/main/java/com/railwayteam/railways/registry/CRIcons.java
+++ common/src/main/java/com/railwayteam/railways/registry/CRIcons.java
@@ -1,10 +1,15 @@
 package com.railwayteam.railways.registry;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.railwayteam.railways.Railways;
 import com.zurrtum.create.client.foundation.gui.AllIcons;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.resources.Identifier;
+import org.joml.Matrix4f;
 
 public class CRIcons extends AllIcons {
     public static final Identifier ICON_ATLAS = Railways.asResource("textures/gui/icons.png");
@@ -31,6 +36,32 @@ public class CRIcons extends AllIcons {
     @Override
     public void render(GuiGraphicsExtractor graphics, int x, int y) {
         graphics.blit(RenderPipelines.GUI_TEXTURED, ICON_ATLAS, x, y, crIconX, crIconY, 16, 16, 256, 256);
+    }
+
+    @Override
+    public void render(GuiGraphicsExtractor graphics, int x, int y, int color) {
+        graphics.blit(RenderPipelines.GUI_TEXTURED, ICON_ATLAS, x, y, crIconX, crIconY, 16, 16, 16, 16, 256, 256, color);
+    }
+
+    @Override
+    public void submit(PoseStack poseStack, SubmitNodeCollector collector, int color) {
+        collector.submitCustomGeometry(poseStack, RenderTypes.text(ICON_ATLAS), new IconRenderState(crIconX, crIconY, color));
+    }
+
+    private record IconRenderState(int iconX, int iconY, int color) implements SubmitNodeCollector.CustomGeometryRenderer {
+        @Override
+        public void render(PoseStack.Pose pose, VertexConsumer consumer) {
+            Matrix4f matrix = pose.pose();
+            int light = 15728880;
+            float u0 = iconX / 256f;
+            float u1 = (iconX + 16) / 256f;
+            float v0 = iconY / 256f;
+            float v1 = (iconY + 16) / 256f;
+            consumer.addVertex(matrix, 0, 1, 0).setColor(color).setUv(u0, v1).setLight(light);
+            consumer.addVertex(matrix, 1, 1, 0).setColor(color).setUv(u1, v1).setLight(light);
+            consumer.addVertex(matrix, 1, 0, 0).setColor(color).setUv(u1, v0).setLight(light);
+            consumer.addVertex(matrix, 0, 0, 0).setColor(color).setUv(u0, v0).setLight(light);
+        }
     }
 
     private static CRIcons next() {
