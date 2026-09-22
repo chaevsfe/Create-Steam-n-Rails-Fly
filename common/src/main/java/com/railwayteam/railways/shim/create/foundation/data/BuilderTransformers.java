ff54ec5a6ea4ac3f906b0be872cc00095e0b7a66
chaevsfe <pie@duck.com>
2026-09-22 15:12:38 -0400

    Tint copycat headstocks and conductor vents by their material and give vents copycat properties

--- common/src/main/java/com/railwayteam/railways/shim/create/foundation/data/BuilderTransformers.java
+++ common/src/main/java/com/railwayteam/railways/shim/create/foundation/data/BuilderTransformers.java
@@ -3,9 +3,14 @@ package com.railwayteam.railways.shim.create.foundation.data;
 import com.railwayteam.railways.shim.registrate.builders.BlockBuilder;
 import com.railwayteam.railways.shim.registrate.util.nullness.NonNullUnaryOperator;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.MapColor;
 
 public class BuilderTransformers {
     public static <B extends Block, P> NonNullUnaryOperator<BlockBuilder<B, P>> copycat() {
-        return builder -> builder;
+        return b -> b.initialProperties(SharedProperties::softMetal)
+            .properties(p -> p.noOcclusion()
+                .mapColor(MapColor.NONE)
+                .isValidSpawn((state, level, pos, type) -> false))
+            .transform(TagGen.axeOrPickaxe());
     }
 }
